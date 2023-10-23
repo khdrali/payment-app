f6d20f16586bb8bc39daacecd7a37956fda92ea5
khdrali <khaidarali48@gmail.com>
2023-10-23 09:16:16 +0700

    integration

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,47 +1,56 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import "../styles/home.css";
 import dashbord from "../images/dashbord.png";
 import frame from "../images/Frame.png";
 import camera from "../images/camera.png";
 import arrow from "../images/arrow-right.png";
 import income from "../images/income.png";
-import arrowBlack from "../images/arrow-right-black.png";
 import transfer from "../images/transfer.png";
 import plus from "../images/plus.png";
 import galery from "../images/gallery.png";
-import axios from "axios";
-import CircularJSON from "circular-json";
 
 function Home() {
+  const [dataExpenses, setDataExpenses]=useState([
+    {
+      name: "Balance",
+      date:"April 2022",
+      pay: "20,129"
+    },
+    {
+      name: "Income",
+      date:"April 2022",
+      pay: "20,129"
+    },
+    {
+      name: "Expenses",
+      date:"April 2022",
+      pay: "20,129"
+    }
+  ])
 
-  let [dataExpenses, setDataExpenses] = React.useState([]);
-  let [dataCard, setDataCard]= React.useState({})
-
-  React.useEffect(()=>{
-    axios.get(`https://testapi.io/api/khdrali/expenses`)
-    .then((res)=>{
-      const result=JSON.parse(CircularJSON.stringify(res.data))
-      setDataExpenses(result)
-      console.log(dataExpenses);
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-
-    axios.get(`https://testapi.io/api/khdrali/mycard`)
-    .then((res)=>{
-      const card = JSON.parse(CircularJSON.stringify(res.data))
-      setDataCard(card)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-    console.log(dataCard);
-    setDataExpenses([1,2,3])
-    console.log(dataExpenses);
-
-  },[])
-
+  const [myCard, setMyCard]=useState([
+    {
+      username:"Syah Bandi",
+      numberCard:"0918 8124 0042 8129",
+      validateCard:"12/20-124",
+      transactionHistory:[{
+        transactionName:"Cash Withdrawal",
+        transactionDate:"13 april 2022",
+        pay:"20,129",
+        status:"out"
+      },{
+        transactionName:"Landing Page Project",
+        transactionDate:"13 april 2022",
+        pay:"2000",
+        status:"in"
+      },{
+        transactionName:"Juni Mobile App project",
+        transactionDate:"13 april 2022",
+        pay:"20,129",
+        status:"in"
+      }]
+    }
+  ])
   return (
     <div className="main">
       <div class="container-fluid">
@@ -58,11 +67,15 @@ function Home() {
                 class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
                 id="menu"
               >
-                <li>
-                  <img src={frame} alt="frame" />
-                  <h5>Lekan Okeowo</h5>
-                  <p>lekanokeowo@gmail.com</p>
-                </li>
+                <div class="row">
+                  <div class="col-3 img-acc">
+                    <img src={frame} alt="frame" />
+                  </div>
+                  <div class="col-9 acc">
+                    <h4>Lekan Okeowo</h4>
+                    <p>demo@gmail.com</p>
+                  </div>
+                </div>
                 <li>
                   <a
                     href="#submenu1"
@@ -76,7 +89,7 @@ function Home() {
             </div>
           </div>
           {/* left side content */}
-          <div class="col lg-7">
+          <div class="col lg-6">
             {/* All Expense */}
             <section id="expanse">
               <div class="shadow-sm p-3 mb-5 bg-body-tertiary rounded all-expanse">
@@ -86,78 +99,31 @@ function Home() {
                   </div>
                   <div class="col-3">
                     <select class="form-select" id="dropdown">
-                      <option selected>
-                        Monthly
-                      </option>
-                      <option value="1">Yen</option>
-                      <option value="2">Rupiah</option>
-                      <option value="3">Euro</option>
+                      <option selected>Monthly</option>
+                      <option value="1">April</option>
+                      <option value="2">Mei</option>
+                      <option value="3">June</option>
                     </select>
                   </div>
                 </div>
                 <div class="row wrap-card">
-                  {/* {dataExpenses.map((item)=>(
+                  {dataExpenses?.map((item)=>(
                   <div class=" col-3 card-select">
                   <div class="row justify-content-between">
                     <div class="col-4">
-                      <img src={camera} alt="camera" width="40px" />
+                      <img src={item?.name === 'Balance' ? camera : item?.name === 'Income' ? income : transfer } alt="camera" width="40px" />
                     </div>
                     <div class="col-4 mt-1">
                       <img src={arrow} alt="arrow" width="25px" />
                     </div>
                   </div>
                   <div class="text">
-                    <h4>Balance</h4>
-                    <p>April 2022</p>
-                    <h3>$20,129</h3>
+                    <h4>{item?.name}</h4>
+                    <p>{item?.date}</p>
+                    <h3>{item?.pay}</h3>
                   </div>
                 </div>  
-                  ))} */}
-                  <div class=" col-3 card-select">
-                    <div class="row justify-content-between">
-                      <div class="col-4">
-                        <img src={camera} alt="camera" width="40px" />
-                      </div>
-                      <div class="col-4 mt-1">
-                        <img src={arrow} alt="arrow" width="25px" />
-                      </div>
-                    </div>
-                    <div class="text">
-                      <h4>Balance</h4>
-                      <p>April 2022</p>
-                      <h3>$20,129</h3>
-                    </div>
-                  </div>
-                  <div class=" col-3 offset-1 card-side">
-                    <div class="row justify-content-between">
-                      <div class="col-4">
-                        <img src={income} alt="camera" width="40px" />
-                      </div>
-                      <div class="col-4 mt-1">
-                        <img src={arrowBlack} alt="arrow" width="25px" />
-                      </div>
-                    </div>
-                    <div class="text">
-                      <h4>Income</h4>
-                      <p>April 2022</p>
-                      <h3>$20,129</h3>
-                    </div>
-                  </div>
-                  <div class=" col-3 offset-1 card-side">
-                    <div class="row justify-content-between">
-                      <div class="col-4">
-                        <img src={transfer} alt="camera" width="40px" />
-                      </div>
-                      <div class="col-4 mt-1">
-                        <img src={arrowBlack} alt="arrow" width="25px" />
-                      </div>
-                    </div>
-                    <div class="text">
-                      <h4>Expense</h4>
-                      <p>April 2022</p>
-                      <h3>$20,129</h3>
-                    </div>
-                  </div>
+                  ))}
                 </div>
               </div>
               <div class="shadow-sm p-3 mb-5 bg-body-tertiary rounded invoice">
@@ -167,7 +133,7 @@ function Home() {
                   </div>
                   <div class="col-2">
                     <button type="button" class="btn">
-                      <img src={plus} width="43px" />
+                      <img src={plus} width="43px" alt="plus" />
                     </button>
                   </div>
                 </div>
@@ -191,8 +157,8 @@ function Home() {
                           <img src={frame} alt="frame" />
                         </div>
                         <div class="col-9">
-                          <h4>Madrani Andi</h4>
-                          <p>madraniandi20@gmail.com</p>
+                          <h4>Josua Nunito</h4>
+                          <p>JoshNunito@gmail.com</p>
                         </div>
                       </div>
                     </div>
@@ -213,8 +179,8 @@ function Home() {
                           <img src={frame} alt="frame" />
                         </div>
                         <div class="col-9">
-                          <h4>Madrani Andi</h4>
-                          <p>madraniandi20@gmail.com</p>
+                          <h4>Josua Nunito</h4>
+                          <p>JoshNunito@gmail.com</p>
                         </div>
                       </div>
                     </div>
@@ -302,7 +268,7 @@ function Home() {
                     </button>
                   </div>
                   <div class="col-4">
-                    <button type="button" class="btn send">
+                    <button type="button" class="btn add">
                       Send Money
                     </button>
                   </div>
@@ -311,15 +277,16 @@ function Home() {
             </section>
           </div>
           {/* right side content */}
-          <div class="col lg-4">
+          <div class="col lg-6">
             <section id="card">
               <h4>My card</h4>
+              {
               <div class="name-card">
                 <div class="container text-center">
                   <div class="row justify-content-between">
                     <div class="col-4">
                       <h6>Name Card</h6>
-                      <p>Syah Bandi</p>
+                      <p>{myCard[0].username}</p>
                     </div>
                     <div class="col-3">
                       <img src={galery} alt="galery" />
@@ -327,10 +294,11 @@ function Home() {
                   </div>
                 </div>
                 <div class="number-card">
-                  <h5>0918 8124 0042 8129</h5>
-                  <p>12/20 - 124</p>
+                  <h5>{myCard[0].numberCard}</h5>
+                  <p>{myCard[0].validateCard}</p>
                 </div>
               </div>
+              }
               <div class="hair-line"></div>
               <div class="container text-center">
                 <div class="row justify-content-between history">
@@ -345,35 +313,41 @@ function Home() {
               <div class="container date">
                 <p>13 April 2022</p>
               </div>
-              <div class="container">
-                <div class="row justify-content-between box">
-                  <div class="col-7">
-                    <h4>Cash Withdrawal</h4>
-                    <p>13 Apr, 2022 </p>
+              {myCard[0].transactionHistory?.map((item,key)=>{
+                return (
+                  <div key={key} class="container">
+                  <div class="row justify-content-between box">
+                    <div class="col-7">
+                      <h4>{item.transactionName}</h4>
+                      <p>{item.transactionDate} </p>
+                    </div>
+                    <div class="col-3">
+                      <h5 style={{color: item?.status === 'out' ? "red" : "#7DD97B"}}>$20,129</h5>
+                    </div>
                   </div>
-                  <div class="col-3">
-                    <h5>$20,129</h5>
+                  {/* <div class="row justify-content-between box">
+                    <div class="col-7">
+                      <h4>Landing Page Project</h4>
+                      <p>13 Apr, 2022 </p>
+                    </div>
+                    <div class="col-3">
+                      <h6>$20,129</h6>
+                    </div>
                   </div>
+                  <div class="row justify-content-between box">
+                    <div class="col-7">
+                      <h4>Juni Mobile App project</h4>
+                      <p>13 Apr, 2022 </p>
+                    </div>
+                    <div class="col-3">
+                      <h6>$20,129</h6>
+                    </div>
+                  </div> */}
                 </div>
-                <div class="row justify-content-between box">
-                  <div class="col-7">
-                    <h4>Landing Page Project</h4>
-                    <p>13 Apr, 2022 </p>
-                  </div>
-                  <div class="col-3">
-                    <h6>$20,129</h6>
-                  </div>
-                </div>
-                <div class="row justify-content-between box">
-                  <div class="col-7">
-                    <h4>Juni Mobile App project</h4>
-                    <p>13 Apr, 2022 </p>
-                  </div>
-                  <div class="col-3">
-                    <h6>$20,129</h6>
-                  </div>
-                </div>
-              </div>
+                )
+              }
+         
+              )}
             </section>
           </div>
         </div>
